0d714968d99429a629c675f76e0f4c0463e5cf64
Alexandru BOSCU <boscu20@gmail.com>
2023-12-25 19:34:21 +0200

    refactored user profile component

--- src/app/components/user-profile/formData.ts
+++ src/app/components/user-profile/formData.ts
@@ -1,14 +1,30 @@
 export const settingsFormData = [
-  { type: 'text', name: 'Username', formControl: 'userName' },
-  { type: 'text', name: 'First name', formControl: 'firstName' },
-  { type: 'text', name: 'Last name', formControl: 'lastName' },
-  { type: 'email', name: 'Email', formControl: 'email' },
-  { type: 'date', name: 'Date of birth', formControl: 'dob' },
-  { type: 'tel', name: 'Phone number', formControl: 'phoneNumber' },
+  {
+    type: 'text',
+    name: 'Username',
+    formControl: 'userName',
+    icon: 'account_box',
+  },
+  {
+    type: 'text',
+    name: 'First name',
+    formControl: 'firstName',
+    icon: 'person',
+  },
+  { type: 'text', name: 'Last name', formControl: 'lastName', icon: 'person' },
+  { type: 'email', name: 'Email', formControl: 'email', icon: 'mail' },
+  { type: 'date', name: 'Date of birth', formControl: 'dob', icon: 'cake' },
+  {
+    type: 'tel',
+    name: 'Phone number',
+    formControl: 'phoneNumber',
+    icon: 'phone',
+  },
 ];
 
 export interface SettingsForm {
   type: string;
   name: string;
   formControl: string;
+  icon: string;
 }
