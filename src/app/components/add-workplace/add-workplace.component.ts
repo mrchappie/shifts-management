7471d9de3dc826e6fd4e437256f7e970b9194d90
Alexandru BOSCU <boscu20@gmail.com>
2024-01-03 17:07:44 +0200

    fixed add workplace when editing user

--- src/app/components/add-workplace/add-workplace.component.ts
+++ src/app/components/add-workplace/add-workplace.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { arrayUnion, arrayRemove } from '@angular/fire/firestore';
 import { FirebaseConfigI, firebaseConfig } from 'firebase.config';
 import { Subscription } from 'rxjs';
@@ -12,7 +12,10 @@ import { StateService } from '../../utils/services/state/state.service';
   styleUrls: ['./add-workplace.component.scss'],
 })
 export class AddWorkplaceComponent {
+  @Input() userIDFromURL!: string;
   currentState!: State;
+  newWorkplace: string = '';
+  userWorkplaces: string[] = [];
 
   // DB Config
   fbConfig: FirebaseConfigI = firebaseConfig;
@@ -22,9 +25,22 @@ export class AddWorkplaceComponent {
   constructor(private state: StateService, private DB: HandleDBService) {}
 
   ngOnInit(): void {
+    // init state
     this.currentState = this.state.getState();
+
+    if (this.userIDFromURL) {
+      // if exists, fetch edited user data
+      this.getUserWorkplaces(this.userIDFromURL);
+    }
+
+    // state subscription to update currentState to the new global state
     this.stateSubscription = this.state.stateChanged.subscribe((newState) => {
       this.currentState = newState;
+
+      if (!this.userIDFromURL) {
+        this.userWorkplaces =
+          this.currentState.currentLoggedFireUser!.userWorkplaces;
+      }
     });
   }
 
@@ -34,24 +50,61 @@ export class AddWorkplaceComponent {
     }
   }
 
-  addWorkplace(workplace: string) {
+  // add workplace from DB
+  addWorkplace() {
+    if (this.newWorkplace === '') return;
     this.DB.updateFirestoreDoc(
       this.fbConfig.dev.usersDB,
-      [this.currentState.currentLoggedFireUser!.id],
-      { userWorkplaces: arrayUnion(workplace) }
+      [this.userIDFromURL ?? this.currentState.currentLoggedFireUser!.id],
+      { userWorkplaces: arrayUnion(this.newWorkplace) }
     );
-    this.updateState();
+    // updates the workplaces array variable
+    if (
+      !this.userWorkplaces.includes(this.newWorkplace) &&
+      this.userIDFromURL
+    ) {
+      this.userWorkplaces.push(this.newWorkplace);
+      this.newWorkplace = '';
+    }
+    // if regular user updates his profile, update the state
+    if (!this.userIDFromURL) {
+      this.updateState();
+      this.newWorkplace = '';
+      return;
+    }
   }
 
+  // remove workplace from db
   removeWorkplace(workplace: string) {
     this.DB.updateFirestoreDoc(
       this.fbConfig.dev.usersDB,
-      [this.currentState.currentLoggedFireUser!.id],
+      [this.userIDFromURL ?? this.currentState.currentLoggedFireUser!.id],
       { userWorkplaces: arrayRemove(workplace) }
     );
-    this.updateState();
+    // updates the workplaces array variable
+    if (
+      !this.userWorkplaces.includes(this.newWorkplace) &&
+      this.userIDFromURL
+    ) {
+      this.userWorkplaces.splice(this.userWorkplaces.indexOf(workplace), 1);
+    }
+    // if regular user updates his profile, update the state
+    if (!this.userIDFromURL) {
+      this.updateState();
+      return;
+    }
   }
 
+  // fetch user workplaces if a user information is modified from admin panel
+  async getUserWorkplaces(userID: string) {
+    const userData = await this.DB.getFirestoreDoc(this.fbConfig.dev.usersDB, [
+      userID,
+    ]);
+
+    this.userWorkplaces = userData?.userWorkplaces;
+  }
+
+  // update state only if the user profile page is accessed
   async updateState() {
     const userSettings = await this.DB.getFirestoreDoc(
       this.fbConfig.dev.usersDB,
