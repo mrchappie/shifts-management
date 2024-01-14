95ca3f0e1969f0dc999711911c0c731cfc365407
Alexandru BOSCU <boscu20@gmail.com>
2024-01-14 10:29:23 +0200

    Update toast.component.ts

--- src/app/components/UI/toast/toast/toast.component.ts
+++ src/app/components/UI/toast/toast/toast.component.ts
@@ -1,22 +1,20 @@
-import {
-  ChangeDetectorRef,
-  Component,
-  Input,
-  Renderer2,
-  ViewChild,
-} from '@angular/core';
+import { NgClass, TitleCasePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { ToastService } from 'src/app/utils/services/toast/toast.service';
 
 @Component({
+  standalone: true,
   selector: 'app-toast',
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.component.scss'],
+  imports: [NgClass, MatIconModule, TitleCasePipe],
 })
 export class ToastComponent {
   @Input() toast!: Toast;
   @Input() index!: number;
 
-  constructor(public toastService: ToastService, private renderer: Renderer2) {}
+  constructor(public toastService: ToastService) {}
 
   closeToast(event: Event, index: number) {
     event.stopPropagation();
