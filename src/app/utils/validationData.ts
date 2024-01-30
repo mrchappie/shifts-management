22bcde2d94b2fea94f596a5a4a1d4fe6ca517301
Alexandru BOSCU <boscu20@gmail.com>
2024-01-30 23:13:01 +0200

    more work on responsivenes

--- src/app/utils/validationData.ts
+++ src/app/utils/validationData.ts
@@ -1,43 +1,43 @@
 export const errorMessages = {
   required: 'This field is required!',
   login: {
-    email: 'Please enter a valid email address.',
-    password: 'Password must be at least 8 characters long.',
+    email: 'Enter a valid email address.',
+    password: 'Password must be 8+ characters.',
   },
   register: {
-    firstName: 'First name must be at least 2 characters long.',
-    lastName: 'Last name must be at least 2 characters long.',
-    email: 'Please enter a valid email address.',
+    firstName: 'First name must be 2+ characters.',
+    lastName: 'Last name must be 2+ characters.',
+    email: 'Enter a valid email address.',
     password: {
-      invalid: '8+ chars, uppercase, lowercase, digit, special char',
+      invalid: '8+ chars, upper, lower, digit, special char.',
       notMatch: 'Passwords do not match.',
     },
     confPass: {
-      invalid: '8+ chars, uppercase, lowercase, digit, special char',
+      invalid: '8+ chars, upper, lower, digit, special char.',
       notMatch: 'Passwords do not match.',
     },
-    dob: 'You must be between 18 and 65 years old.',
+    dob: 'Age requirement: 18-65 years.',
   },
   credentials: {
-    email: 'Please enter a valid email address.',
+    email: 'Enter a valid email address.',
     password: {
-      invalid: '8+ chars, uppercase, lowercase, digit, special char.',
-      short: 'Password must be at least 8 characters long.',
+      invalid: '8+ chars, upper, lower, digit, special char.',
+      short: 'Password must be 8+ characters.',
       notMatch: 'Passwords do not match.',
     },
   },
   shift: {
-    date: 'Please enter a valid date for the shift.',
-    start: 'Please enter a valid start time for the shift.',
-    end: 'Please enter a valid end time for the shift.',
+    date: 'Enter a valid date for the shift.',
+    start: 'Enter a valid start time for the shift.',
+    end: 'Enter a valid end time for the shift.',
     workplace: 'Workplace is required.',
-    wage: 'Please enter a valid wage for the shift.',
+    wage: 'Enter a valid wage for the shift.',
   },
   profile: {
-    firstName: 'First name must be at least 2 characters long.',
-    lastName: 'Last name must be at least 2 characters long.',
-    dob: 'You must be between 18 and 65 years old.',
-    phoneNumber: 'Please enter a valid phone number (10 digits).',
+    firstName: 'First name must be 2+ characters.',
+    lastName: 'Last name must be 2+ characters.',
+    dob: 'Age requirement: 18-65 years.',
+    phoneNumber: 'Enter valid 10-digit phone number.',
   },
 };
 
